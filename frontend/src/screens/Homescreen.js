c002e563c7d6608d3f2e81efc154c7782b3d74ff
Dika Aditia <aditiaaadicka@gmail.com>
2020-10-15 20:41:14 +0700

    setting up express js and connect to frontend

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,19 +1,37 @@
-import React from 'react'
-import products from '../products'
+import React, {useState, useEffect} from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
 
 export default function Homescreen(props) {
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            setLoading(true)
+            const { data } = await axios.get('/api/products')
+            setProducts(data)
+            setLoading(false)
+        }
+
+        fetchProduct()
+    }, []);
+
     return (
         <>
-            <h1>Latest products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+            {loading ? <p>Loading.....</p> : 
+                <>
+                    <h1>Latest products</h1>
+                    <Row>
+                        {products.map(product => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                </>
+            }
         </>
     )
 }
